485b042a8d56c60a578faa5cf6519c91fa192e02
Kristy <kristy010@yandex.ru>
2023-06-21 15:45:25 +0300

    js files

--- src/js/script.js
+++ src/js/script.js
@@ -76,4 +76,6 @@ $(document).ready(function(){
     valideForms('#consultation-form');
     valideForms('#consultation form');
     valideForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99"); //mask for 
   });
